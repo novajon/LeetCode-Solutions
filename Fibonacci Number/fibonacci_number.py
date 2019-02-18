db60fdb66665f84bcc7f5da035fedb03792967ee
novajon <jonathan.janke@ghcs.de>
2019-02-18 23:10:26 +0100

    Fibonacci Number

---
+++ Fibonacci Number/fibonacci_number.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/fibonacci-number/
+
+class Solution:
+    def fib(self, N: 'int') -> 'int':
+        if N == 0:
+            return 0
+        elif N == 1:
+            return 1
+        else:
+            return self.fib(N-1) + self.fib(N-2)
+        
