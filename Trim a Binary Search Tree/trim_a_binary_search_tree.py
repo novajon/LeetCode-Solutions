686ea58e3d0711286c6762bf7fb5b46020627350
novajon <jonathan.janke@ghcs.de>
2019-04-02 10:10:30 +0200

    Trim a Binary Search Tree

---
+++ Trim a Binary Search Tree/trim_a_binary_search_tree.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/trim-a-binary-search-tree/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return
+        
+        if root.val < L:
+            return self.trimBST(root.right, L, R)
+        elif root.val > R:
+            return self.trimBST(root.left, L, R)
+        
+        if root.left:
+            root.left = self.trimBST(root.left, L, R)
+        if root.right:
+            root.right = self.trimBST(root.right, L, R)
+        
+        return root
