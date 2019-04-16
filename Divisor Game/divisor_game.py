7c69efdae4ed40f9278e0b821ab3658925cb0945
novajon <jonathan.janke@ghcs.de>
2019-04-16 08:39:45 +0200

    Divisor Game

---
+++ Divisor Game/divisor_game.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/divisor-game/
+
+class Solution(object):
+    def divisorGame(self, N):
+        """
+        :type N: int
+        :rtype: bool
+        """
+        
+        return N%2==0
